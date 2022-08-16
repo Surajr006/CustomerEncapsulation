face6b6a7079518e802596247e7fd25c9140adea
Suraj R <surajr006@gmail.com>
2022-08-16 13:26:35 +0530

    second commit

--- src/customerinfo/CustomerImpl.java
+++ src/customerinfo/CustomerImpl.java
@@ -1,4 +1,10 @@
 package customerinfo;
 
+import customermethod.Customer;
+
 public class CustomerImpl {
+    public static void main(String[] args) {
+        Customer cust1=new Customer();
+
+    }
 }
--- src/customermethod/Customer.java
+++ src/customermethod/Customer.java
@@ -1,4 +1,22 @@
 package customermethod;
 
 public class Customer {
+    String name;
+    String address;
+    long mobileNo;
+    boolean isPremium;
+    public Customer()
+    {
+        System.out.println("No argument constructor called");
+    }
+    Customer(String name,String address,long mobileNo,boolean isPremium)
+    {
+        this.name=name;
+        this.address=address;
+        this.mobileNo=mobileNo;
+        this.isPremium=isPremium;
+    }
+    void displayCustomer(){
+
+    }
 }
