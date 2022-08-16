bdb53ecadfa30eb206b0800524da97e34a3ca6cd
Suraj R <surajr006@gmail.com>
2022-08-16 14:21:05 +0530

    final commit

--- src/customerinfo/CustomerImpl.java
+++ src/customerinfo/CustomerImpl.java
@@ -5,6 +5,18 @@ import customermethod.Customer;
 public class CustomerImpl {
     public static void main(String[] args) {
         Customer cust1=new Customer();
+        cust1.setName("Suraj");
+        cust1.setAddress("India");
+        cust1.setMobileNo(9445367302L);
+        cust1.setPremium(true);
 
+        Customer cust2=new Customer("Ram","Pakistan",9558754628L,false);
+
+        Customer temp;
+        temp=cust1;
+        cust1=cust2;
+        cust2=temp;
+        cust1.displayCustomer();
+        cust2.displayCustomer();
     }
 }
--- src/customermethod/Customer.java
+++ src/customermethod/Customer.java
@@ -5,18 +5,35 @@ public class Customer {
     String address;
     long mobileNo;
     boolean isPremium;
-    public Customer()
+     public Customer()
     {
         System.out.println("No argument constructor called");
     }
-    Customer(String name,String address,long mobileNo,boolean isPremium)
+    //getter methods
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public void setMobileNo(long mobileNo){
+        this.mobileNo=mobileNo;
+    }
+    public void setPremium(boolean isPremium){
+        this.isPremium=isPremium;
+    }
+    public Customer(String name, String address, long mobileNo, boolean isPremium)
     {
         this.name=name;
         this.address=address;
         this.mobileNo=mobileNo;
         this.isPremium=isPremium;
     }
-    void displayCustomer(){
-
+    public void displayCustomer(){
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+        System.out.println("mobileNo = " + mobileNo);
+        System.out.println("Premium = " + isPremium);
     }
 }
